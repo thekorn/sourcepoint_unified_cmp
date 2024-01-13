8452d67e4ab7655c2e9244d7aa5bc9fcd76b7942
Markus Korn <markus.korn@gmail.com>
2024-01-13 12:07:18 +0100

    restructuring of the repository & started upgrading android

---
+++ packages/sourcepoint_unified_cmp_ios/test/sourcepoint_unified_cmp_ios_test.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sourcepoint_unified_cmp_ios/sourcepoint_unified_cmp_ios.dart';
+import 'package:sourcepoint_unified_cmp_platform_interface/sourcepoint_unified_cmp_platform_interface.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  group('SourcepointUnifiedCmpIOS', () {
+    const kPlatformName = 'iOS';
+    late SourcepointUnifiedCmpIOS sourcepointUnifiedCmp;
+    late List<MethodCall> log;
+
+    setUp(() async {
+      sourcepointUnifiedCmp = SourcepointUnifiedCmpIOS();
+
+      log = <MethodCall>[];
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+          .setMockMethodCallHandler(sourcepointUnifiedCmp.methodChannel,
+              (methodCall) async {
+        log.add(methodCall);
+        switch (methodCall.method) {
+          case 'getPlatformName':
+            return kPlatformName;
+          default:
+            return null;
+        }
+      });
+    });
+
+    test('can be registered', () {
+      SourcepointUnifiedCmpIOS.registerWith();
+      expect(SourcepointUnifiedCmpPlatform.instance,
+          isA<SourcepointUnifiedCmpIOS>());
+    });
+
+    test('getPlatformName returns correct name', () async {
+      final name = await sourcepointUnifiedCmp.getPlatformName();
+      expect(
+        log,
+        <Matcher>[isMethodCall('getPlatformName', arguments: null)],
+      );
+      expect(name, equals(kPlatformName));
+    });
+  });
+}
---
+++ packages/sourcepoint_unified_cmp_platform_interface/lib/sourcepoint_unified_cmp_platform_interface.dart
@@ -0,0 +1,34 @@
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+import 'package:sourcepoint_unified_cmp_platform_interface/src/method_channel_sourcepoint_unified_cmp.dart';
+
+/// The interface that implementations of sourcepoint_unified_cmp must implement.
+///
+/// Platform implementations should extend this class
+/// rather than implement it as `SourcepointUnifiedCmp`.
+/// Extending this class (using `extends`) ensures that the subclass will get
+/// the default implementation, while platform implementations that `implements`
+///  this interface will be broken by newly added [SourcepointUnifiedCmpPlatform] methods.
+abstract class SourcepointUnifiedCmpPlatform extends PlatformInterface {
+  /// Constructs a SourcepointUnifiedCmpPlatform.
+  SourcepointUnifiedCmpPlatform() : super(token: _token);
+
+  static final Object _token = Object();
+
+  static SourcepointUnifiedCmpPlatform _instance =
+      MethodChannelSourcepointUnifiedCmp();
+
+  /// The default instance of [SourcepointUnifiedCmpPlatform] to use.
+  ///
+  /// Defaults to [MethodChannelSourcepointUnifiedCmp].
+  static SourcepointUnifiedCmpPlatform get instance => _instance;
+
+  /// Platform-specific plugins should set this with their own platform-specific
+  /// class that extends [SourcepointUnifiedCmpPlatform] when they register themselves.
+  static set instance(SourcepointUnifiedCmpPlatform instance) {
+    PlatformInterface.verify(instance, _token);
+    _instance = instance;
+  }
+
+  /// Return the current platform name.
+  Future<String?> getPlatformName();
+}
