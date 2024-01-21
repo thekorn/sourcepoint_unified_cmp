7d591e550bcffde91269d9a2377b03129d522071
Markus Korn <markus.korn@gmail.com>
2024-01-21 14:03:39 +0100

    feat(SourcepointUnifiedCmpPlugin.swift): integrate ConsentViewController for GDPR and CCPA compliance - Add ConsentViewController to handle user consent for GDPR and CCPA - Implement SPDelegate methods to handle consent events - Add error handling for consent process - Add support for loading privacy manager - Change SourcepointUnifiedCmpPlugin superclass from NSObject to UIViewController to present consent UI - Add consentManager to manage consent process - Load consent message on plugin initialization - Add logging for debugging purposes This change is necessary to comply with GDPR and CCPA regulations.

--- packages/sourcepoint_unified_cmp_ios/ios/Classes/SourcepointUnifiedCmpPlugin.swift
+++ packages/sourcepoint_unified_cmp_ios/ios/Classes/SourcepointUnifiedCmpPlugin.swift
@@ -1,12 +1,28 @@
 import Flutter
+import ConsentViewController
 import UIKit
 
 // This extension of Error is required to do use FlutterError in any Swift code.
 extension FlutterError: Error {}
 
-public class SourcepointUnifiedCmpPlugin: NSObject, FlutterPlugin, SourcepointUnifiedCmpHostApi {
+public class SourcepointUnifiedCmpPlugin: UIViewController, FlutterPlugin, SourcepointUnifiedCmpHostApi {
+    
+    private var consentManager: SPSDK!
+    
     func loadMessage(accountId: Int64, propertyId: Int64, propertyName: String, pmId: String, messageLanguage: HostAPIMessageLanguage, campaignsEnv: HostAPICampaignsEnv, messageTimeout: Int64, runGDPRCampaign: Bool, runCCPACampaign: Bool, completion: @escaping (Result<HostAPISPConsent, Error>) -> Void) {
         NSLog(">>>>> WE LOAD MESSAGE")
+    consentManager = { SPConsentManager(
+        accountId: Int(accountId),
+            propertyId: Int(propertyId),
+            propertyName: try! SPPropertyName(propertyName),
+            campaigns: SPCampaigns(
+                gdpr: SPCampaign(),
+                ccpa: SPCampaign(),
+                ios14: SPCampaign()
+            ),
+            delegate: self
+        )}()
+        consentManager.loadMessage()
     }
     
     func loadPrivacyManager(pmId: String, pmTab: HostAPIPMTab, campaignType: HostAPICampaignType, messageType: HostAPIMessageType, completion: @escaping (Result<Void, Error>) -> Void) {
@@ -15,7 +31,69 @@ public class SourcepointUnifiedCmpPlugin: NSObject, FlutterPlugin, SourcepointUn
     
     public static func register(with registrar: FlutterPluginRegistrar) {
         let messenger : FlutterBinaryMessenger = registrar.messenger()
-            let api : SourcepointUnifiedCmpHostApi & NSObjectProtocol = SourcepointUnifiedCmpPlugin.init()
+        let api : SourcepointUnifiedCmpHostApi & NSObjectProtocol = SourcepointUnifiedCmpPlugin.init()
         SourcepointUnifiedCmpHostApiSetup.setUp(binaryMessenger: messenger, api: api);
+        ///registrar.addApplicationDelegate(self)
+    }
+}
+
+extension SourcepointUnifiedCmpPlugin: SPDelegate {
+    func onSPUIReady(_ controller: SPMessageViewController) {
+        NSLog("onSPUIReady, sp message view controller")
+        controller.modalPresentationStyle = .overFullScreen
+        present(controller, animated: true)
+    }
+    
+    public func onSPUIReady(_ controller: UIViewController) {
+        NSLog("onSPUIReady, ui view controller")
+        controller.modalPresentationStyle = .overFullScreen
+        DispatchQueue.main.async { [weak self] in
+            self?.present(controller, animated: true)
+        }
+        //present(controller, animated: true)
+        /// UIApplication.shared.keyWindow?.rootViewController?.present(consentManager as! UIViewController, animated: true, completion: nil)
+    }
+
+    func onAction(_ action: SPAction, from controller: SPMessageViewController) {
+        NSLog("onAction, sp message view controller")
+        NSLog("\(action)")
+    }
+    
+    public func onAction(_ action: SPAction, from controller: UIViewController) {
+        NSLog("onAction, ui view controller")
+        NSLog("\(action)")
+    }
+
+    func onSPUIFinished(_ controller: SPMessageViewController) {
+        dismiss(animated: true)
+    }
+    
+    
+    public func onSPUIFinished(_ controller: UIViewController) {
+        NSLog("onSPUIFinished")
+    }
+    
+    public func onSPFinished(userData: SPUserData) {
+        NSLog("onSPFinished")
+        NSLog("sourcepoint sdk done")
+    }
+
+    public func onConsentReady(userData: SPUserData) {
+        NSLog("onConsentReady")
+        NSLog("onConsentReady:", userData)
+        // checking if a gdpr vendor is consented
+        userData.gdpr?.consents?.vendorGrants["myVendorId"]?.granted
+
+        // checking if a ccpa vendor is rejected (on ccpa, vendors are accepted by default)
+        userData.ccpa?.consents?.rejectedVendors.contains("myVendorId")
+    }
+
+    public func onError(error: SPError) {
+        NSLog("onError")
+        NSLog("Something went wrong: ", error)
+    }
+    
+    public func onSPNativeMessageReady(_ message: SPNativeMessage) {
+        NSLog("onSPNativeMessageReady")
     }
 }
