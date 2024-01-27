438d326a8a6c08854bb4f9a3ea2e0f476c3fa06f
Markus Korn <markus.korn@gmail.com>
2024-01-27 17:05:09 +0100

    ✨ feat: add release.sh script to automate versioning and publishing of packages
    This script checks for correct arguments, copies documentation, versions packages using melos, and publishes them.

---
+++ release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+#!/bin/bash
+
+if [[ "$1" != "patch" && "$1" != "minor" && "$1" != "major" ]]; then
+  echo "Error: Argument must be 'patch', 'minor', or 'major'"
+  exit 1  # Exit with a status of 1 to indicate an error
+fi
+
+
+echo "copy docs to sourcepoint_unified_cmp..."
+
+cp README.md packages/sourcepoint_unified_cmp/README.md
+cp -r docs packages/sourcepoint_unified_cmp/docs
+
+git add .
+
+melos version \
+    -V sourcepoint_unified_cmp:$1 \
+    -V sourcepoint_unified_cmp_android:$1 \
+    -V sourcepoint_unified_cmp_ios:$1 \
+    -V sourcepoint_unified_cmp_platform_interface:$1 \
+    -r
+
+melos publish --no-dry-run
