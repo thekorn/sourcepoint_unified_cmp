5990a0a2c8075501f712576a76ce6fe8c9fb00c3
Markus Korn <markus.korn@gmail.com>
2024-02-26 01:55:27 +0100

    🎨 style(main.dart): add MaterialApp properties for better UI experience 🔨 refactor(main.dart): wrap SourcepointUnifiedCMPBuilder with Scaffold for better structure and readability 🐛 fix(main.dart): handle error state in SourcepointUnifiedCMPBuilder to provide feedback to user when error occurs

--- packages/sourcepoint_unified_cmp/example/lib/main.dart
+++ packages/sourcepoint_unified_cmp/example/lib/main.dart
@@ -11,7 +11,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: SourcepointUnifiedCMPBuilderExample());
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SourcepointUnifiedCMPBuilderExample(),
+    );
   }
 }
 
@@ -57,74 +64,75 @@ class _SourcepointUnifiedCMPBuilderExampleState
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTextStyle(
-      style: Theme.of(context).textTheme.displayMedium!,
-      textAlign: TextAlign.center,
-      child: SourcepointUnifiedCMPBuilder(
-        controller: _controller,
-        builder: (BuildContext context, AsyncSnapshot<SPConsent> snapshot) {
-          List<Widget> children;
-          if (snapshot.hasData) {
-            final consent = snapshot.data;
-            debugPrint('user consent has been loaded:');
-            debugPrint('   grants: ${consent?.gdpr?.grants}');
-            debugPrint('euconsent: ${consent?.gdpr?.euconsent}');
-            children = <Widget>[
-              const Icon(
-                Icons.check_circle_outline,
-                color: Colors.green,
-                size: 60,
-              ),
-              const Padding(
-                padding: EdgeInsets.only(top: 16),
-                child: Text('Result: we got initial consent'),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16),
-                child: TextButton(
-                  onPressed: () {
-                    _controller.loadPrivacyManager(
-                      pmId: '122058',
-                    );
-                  },
-                  child: const Text('Load Privacy Manager'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('webview sample'),
+      ),
+      body: Center(
+        child: SourcepointUnifiedCMPBuilder(
+          controller: _controller,
+          builder: (BuildContext context, AsyncSnapshot<SPConsent> snapshot) {
+            List<Widget> children;
+            if (snapshot.hasData) {
+              final consent = snapshot.data;
+              debugPrint('user consent has been loaded:');
+              debugPrint('   grants: ${consent?.gdpr?.grants}');
+              debugPrint('euconsent: ${consent?.gdpr?.euconsent}');
+              children = <Widget>[
+                const Icon(
+                  Icons.check_circle_outline,
+                  color: Colors.green,
+                  size: 60,
                 ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 16),
+                  child: Text('Result: we got initial consent'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: TextButton(
+                    onPressed: () {
+                      _controller.loadPrivacyManager(
+                        pmId: '122058',
+                      );
+                    },
+                    child: const Text('Load Privacy Manager'),
+                  ),
+                ),
+              ];
+            } else if (snapshot.hasError) {
+              children = <Widget>[
+                const Icon(
+                  Icons.error_outline,
+                  color: Colors.red,
+                  size: 60,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Text('Error: ${snapshot.error}'),
+                ),
+              ];
+            } else {
+              children = const <Widget>[
+                SizedBox(
+                  width: 60,
+                  height: 60,
+                  child: CircularProgressIndicator(),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16),
+                  child: Text('Awaiting consent...'),
+                ),
+              ];
+            }
+            return SingleChildScrollView(
+              child: Column(
+                children: children,
               ),
-            ];
-          } else if (snapshot.hasError) {
-            debugPrint('error loading user consent:');
-            debugPrint('error: ${snapshot.error}');
-            children = <Widget>[
-              const Icon(
-                Icons.error_outline,
-                color: Colors.red,
-                size: 60,
-              ),
-              const Padding(
-                padding: EdgeInsets.only(top: 16),
-                child: Text('Error: error loading the consent'),
-              ),
-            ];
-          } else {
-            children = const <Widget>[
-              SizedBox(
-                width: 60,
-                height: 60,
-                child: CircularProgressIndicator(),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 16),
-                child: Text('Awaiting consent...'),
-              ),
-            ];
-          }
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: children,
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
