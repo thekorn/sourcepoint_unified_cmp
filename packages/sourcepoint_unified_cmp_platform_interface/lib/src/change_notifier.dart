cb0ff22dc47b50061f2eb4d907ff4039e31a24fc
Markus Korn <markus.korn@gmail.com>
2024-04-09 02:58:40 +0200

    ♻️ (change_notifier.dart): Change ConsentChangeNotifier class to abstract to enforce implementation in subclasses 📝 (change_notifier.dart): Remove unnecessary comment line for cleaner code documentation

--- packages/sourcepoint_unified_cmp_platform_interface/lib/src/change_notifier.dart
+++ packages/sourcepoint_unified_cmp_platform_interface/lib/src/change_notifier.dart
@@ -4,7 +4,7 @@ import 'package:sourcepoint_unified_cmp_platform_interface/sourcepoint_unified_c
 /// A class that extends [ChangeNotifier] and provides a way to notify listeners
 /// when the consent changes.
 /// This is triggered by the platform implementation when the consent is ready.
-class ConsentChangeNotifier extends ChangeNotifier {
+abstract class ConsentChangeNotifier extends ChangeNotifier {
   /// Represents the consent for Sourcepoint Unified CMP.
   SPConsent? consent;
 
@@ -12,7 +12,6 @@ class ConsentChangeNotifier extends ChangeNotifier {
   ///
   /// This method is used to update the consent with the new consent information
   /// provided in the [newConsent] parameter.
-  /// ```
   void updateConsent(SPConsent newConsent) {
     consent = newConsent;
     notifyListeners();
