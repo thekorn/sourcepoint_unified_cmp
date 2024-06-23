faf96d251c0fd65dea4874f36abee10c9941ebd7
Markus Korn <markus.korn@gmail.com>
2024-06-23 20:35:49 +0200

    Refine Pigeon config and update cspell settings
    - Streamline Pigeon configurations for both Android and iOS platforms.
    - Update spell check settings by adding new terms to the dictionary.

--- packages/sourcepoint_unified_cmp_android/pigeons/messages.dart
+++ packages/sourcepoint_unified_cmp_android/pigeons/messages.dart
@@ -165,14 +165,6 @@ class SPConfig {
 @ConfigurePigeon(
   PigeonOptions(
     dartOut: 'lib/src/messages.g.dart',
-    //dartTestOut: 'test/test_android_webview.g.dart',
-    dartOptions: DartOptions(
-      copyrightHeader: <String>[
-        //'Copyright 2013 The Flutter Authors. All rights reserved.',
-        //'Use of this source code is governed by a BSD-style license that ',
-        //'can be found in the LICENSE file.',
-      ],
-    ),
     kotlinOut:
         'android/src/main/kotlin/de/thekorn/sourcepoint/unified/cmp/SourcepointUnifiedCmp.g.kt',
     kotlinOptions: KotlinOptions(errorClassName: 'HostApiFlutterError'),
