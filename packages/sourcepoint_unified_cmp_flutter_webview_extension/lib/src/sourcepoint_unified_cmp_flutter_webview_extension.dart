bfce6531f3b5474ffd756ea96e795b4db88816d6
Markus Korn <markus.korn@gmail.com>
2024-07-27 23:55:52 +0200

    feat: Enhance Sourcepoint CMP WebViewController integration
    - Update documentation to reflect changes in consent preloading approach.

--- packages/sourcepoint_unified_cmp_flutter_webview_extension/lib/src/sourcepoint_unified_cmp_flutter_webview_extension.dart
+++ packages/sourcepoint_unified_cmp_flutter_webview_extension/lib/src/sourcepoint_unified_cmp_flutter_webview_extension.dart
@@ -1,12 +1,21 @@
-// ignore_for_file: prefer_single_quotes, avoid_escaping_inner_quotes, lines_longer_than_80_chars, public_member_api_docs
-
 import 'package:sourcepoint_unified_cmp_platform_interface/sourcepoint_unified_cmp_platform_interface.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 const _eventSPPreload = 'sp.loadConsent';
 const _eventSPReady = 'sp.readyForConsent';
 
+/// Extension on [WebViewController] to provide additional functionality for
+/// Sourcepoint Unified CMP.
+/// This extension adds a method to inject the [SPConsent] to the
+/// [WebViewController] class.
 extension SourcepointUnifiedCmpWebViewController on WebViewController {
+  /// Preloads the consent for the Sourcepoint Unified CMP Flutter WebView.
+  ///
+  /// This method asynchronously preloads the Sourcepoint Unified Consent.
+  /// The [consent] parameter is required and represents the consent to be
+  /// preloaded.
+  ///
+  /// Throws an exception if an error occurs during the preloading process.
   Future<void> preloadConsent({required SPConsent consent}) async {
     assert(consent.webConsents != null, 'webConsents cannot be null');
     final postMessageString = '''
