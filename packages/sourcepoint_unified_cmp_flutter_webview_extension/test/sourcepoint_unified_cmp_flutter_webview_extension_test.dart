9b30c04face26a89c539e0562adbf24299fb76fa
Markus Korn <markus.korn@gmail.com>
2024-08-24 05:40:56 +0200

    chore: add dummy test to the webview extension

---
+++ packages/sourcepoint_unified_cmp_flutter_webview_extension/test/sourcepoint_unified_cmp_flutter_webview_extension_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {});
+
+  test('dummy', () async {
+    // FIXME(mkorn): add some tests here
+  });
+}
