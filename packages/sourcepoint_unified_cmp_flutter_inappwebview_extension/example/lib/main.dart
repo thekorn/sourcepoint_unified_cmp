37b3ae12722eeddf28adc83aec306ca453428272
Markus Korn <markus.korn@gmail.com>
2024-09-06 11:12:51 +0200

    fix import order

--- packages/sourcepoint_unified_cmp_flutter_inappwebview_extension/example/lib/main.dart
+++ packages/sourcepoint_unified_cmp_flutter_inappwebview_extension/example/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:sourcepoint_unified_cmp/sourcepoint_unified_cmp.dart';
 import 'package:sourcepoint_unified_cmp_flutter_inappwebview_extension/sourcepoint_unified_cmp_flutter_inappwebview_extension.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
